e724b3a9fbd04a7d8475ba7c5a60040d0162ec94
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-10-04 22:16:05 +0530

    delete route path set

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -19,19 +19,26 @@ export const GET = async (request, { params }) => {
 
 export const PATCH = async (request, { params }) => {
     const { prompt, tag } = await request.json();
+
     try {
         await connectToDB();
 
+        // Find the existing prompt by ID
         const existingPrompt = await Prompt.findById(params.id);
-        if (!existingPrompt) return new Response("Promp not found", { status: 404 });
 
+        if (!existingPrompt) {
+            return new Response("Prompt not found", { status: 404 });
+        }
+
+        // Update the prompt with new data
         existingPrompt.prompt = prompt;
         existingPrompt.tag = tag;
 
         await existingPrompt.save();
-        return new Response(JSON.stringify(existingPrompt), { status: 200 });
+
+        return new Response("Successfully updated the Prompts", { status: 200 });
     } catch (error) {
-        return new Response("Failed to update  prompts", { status: 500 });
+        return new Response("Error Updating Prompt", { status: 500 });
     }
 };
 
